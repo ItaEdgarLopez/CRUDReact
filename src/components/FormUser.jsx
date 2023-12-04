2c1e86fb91c2b0e6811cb804bdc173807069cd4d
ItaEdgarLopez <ita.edgar.lopez@gmail.com>
2023-12-03 23:32:42 -0600

    Add viewList

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -4,6 +4,9 @@ import { useState } from "react";
 export function FormUser (){
     const [nombre, setNombre] = useState('');
     const [listaNombres, setListaNombres] = useState([]);
+    const[modoEdicion, setModoEdicion] = useState(false);
+    const [error, setError] = useState(null);
+    
 
 
     function obersevable (e)
@@ -13,8 +16,32 @@ export function FormUser (){
 
     function SaveNames(event){
         event.preventDefault()
+
+     if (!nombre.trim()) {
+        setError('El campo nombre está vacío');
+        return;
+}
         setListaNombres([...listaNombres, nombre]);
-        console.log('Registrado')
+        setNombre('')
+        setError(null)
+    }
+
+    const deleteName=(nombre) =>{
+        const newArray = listaNombres.filter( name => name !==nombre)
+        setListaNombres(newArray)
+    }
+
+    const update = (nombreEditar) => {
+        setModoEdicion(true)
+        setNombre(nombreEditar)
+    }
+
+    const editName = (e) => {
+        e.preventDefault();
+        const newArray = listaNombres.map((name) => (name ? nombre : name));
+        setListaNombres(newArray);
+        setModoEdicion(false); // Agrega esto para salir del modo de edición después de editar
+        setNombre(''); // Agrega esto para limpiar el campo de entrada después de la edición
     }
 
     return(
@@ -24,12 +51,31 @@ export function FormUser (){
             <div className="row">
                 <div className="col">
                     <h2>Listado</h2>
+                    <ul className="list-group">
+                    {
+                        listaNombres.map( (nombre, index) => (
+                            <li className="list-group-item" key={index}> 
+                                {nombre}
+                                <button 
+                                className="btn btn-primary float-end"
+                                onClick={()=> update(nombre)}
+                                >Editar</button>
+                                <button 
+                                className="btn btn-danger float-end"
+                                onClick={() =>deleteName(nombre)}
+                                >Eliminar</button>
+
+                             </li>
+                            
+                        ))
+                    }
+                    </ul>
                 </div>
 
                 <div className="col">
                     <h2>Agregando nombres</h2>
 
-                    <form onSubmit={SaveNames} className="form-group">
+                    <form onSubmit={modoEdicion ? editName :  SaveNames} className="form-group">
                         <input 
                         onChange={(e)=> obersevable(e)} 
                         value={nombre}
@@ -37,8 +83,21 @@ export function FormUser (){
                         type="text" 
                         placeholder="Nombre" 
                         /> 
-                        <input className="btn btn-primary" type="submit"  placeholder=""/>
+                        <input 
+                        value={modoEdicion ? 'Editar Nombre' : 'Agregar Nombre'}
+                        className="btn btn-primary" type="submit"  placeholder=""/>
                     </form>
+                    {
+                        error !=null ? (
+                            
+                            <div className="alert alert-danger"> {error} </div>
+                        ):
+                            (
+                                <div></div>
+                            )
+                    }
+                 
+
                 </div>
             </div>
 
